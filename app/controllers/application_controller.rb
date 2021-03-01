2e83611697218a205db57d168d630be04e527e34
satodd1967 <seth.todd@outlook.com>
2021-02-28 20:19:29 -0600

    Took out the helpers do from the application controller and just left the methods without it.  Took out example.com from the config initializers cors.rb file and replaced it with * to allow all requests for development.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::API
-    helpers do
-        def current_user
-            User.first
-        end
 
-        def logged_in?
-            !!current_user
-        end
+    def current_user
+        User.first
     end
+    def logged_in?
+        !!current_user
+    end
+
 end
