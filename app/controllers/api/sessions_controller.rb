c104429e7fb417d80b29c4bcda974ab9091703fb
satodd1967 <seth.todd@outlook.com>
2021-03-03 15:31:30 -0600

    updated the sessions controller to login a user and have an error render if the user didn't authenticate.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,15 @@
 class Api::SessionsController < ApplicationController
 
     def create
-       binding.pry
+       @user = User.find_by(email: params[:session][:email])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            render json: @user
+        else
+            render json: {
+                error: "Invalid Credentials"
+            }
+        end
     end
 
 end
