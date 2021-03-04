e385300545fa25308f4c509e5c1584bdee608b2a
satodd1967 <seth.todd@outlook.com>
2021-03-03 19:24:33 -0600

    Updated the sessions controller to handle logut.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -26,7 +26,7 @@ class Api::SessionsController < ApplicationController
         session.clear
         render json: {
             notice: "logged out"
-        }
+        }, status: :ok
     end
 
 end
