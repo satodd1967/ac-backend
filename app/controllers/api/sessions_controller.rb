6b516ddf8c7e5ef960344b28a5f6cecf797973db
satodd1967 <seth.todd@outlook.com>
2021-03-03 20:09:45 -0600

    Updated the sessions controller to use the users serializer to eliminate the password digest from being passed back to the browser.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::SessionsController < ApplicationController
        @user = User.find_by(email: params[:session][:email])
         if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
-            render json: @user
+            render json: UserSerializer.new(@user)
         else
             render json: {
                 error: "Invalid Credentials"
@@ -14,7 +14,7 @@ class Api::SessionsController < ApplicationController
 
     def get_current_user
         if logged_in?
-            render json: current_user
+            render json: UserSerializer.new(current_user)
         else
             render json: {
                 error: "No one logged in"
