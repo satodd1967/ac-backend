5ee4bdf6c8b82a2e2d99df32cf4040c361f88a47
satodd1967 <seth.todd@outlook.com>
2021-03-22 20:58:09 -0500

    Had to update the user serializer to restructure the way that logs and log_Scores were presented and include some data relationships that weren't there like the challenge name.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,7 +6,38 @@ class UserSerializer
     :image,
     :owned_challenges,
     :logs,
-    :log_scores,
     :challenge_goals,
-    :challenges 
+    :challenges
+
+    attribute :logs do |user|
+      user.logs.map do |log|
+        {
+          id: log.id,
+          user_id: log.user_id,
+          log_date: log.log_date,
+          worked_out: log.worked_out,
+          tracked_food: log.tracked_food,
+          weight: log.weight,
+          body_fat: log.body_fat,
+          active_calories: log.active_calories,
+          calories: log.calories,
+          log_scores: log.log_scores.map do |ls|
+            {
+              id: ls.id,
+              points_worked_out: ls.points_worked_out,
+              points_tracked_food: ls.points_tracked_food,
+              points_met_calorie_goal: ls.points_met_calorie_goal,
+              points_maintain_weight: ls.points_maintain_weight,
+              points_maintain_body_fat: ls.points_maintain_body_fat,
+              points_met_active_calorie_goal: ls.points_met_active_calorie_goal,
+              total_points: ls.total_points,
+              challenge: ls.challenge_goal.challenge.name
+            }
+          end
+        }
+      end
+    end
+
 end
+
+
